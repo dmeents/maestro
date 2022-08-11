2cc8537569b5e3bada589625e49fca18670c20ac
David Meents <david@allido.io>
2022-08-11 11:18:15 -0700

    fix: fix invalid semantic-release config

--- src/semantic-release/index.ts
+++ src/semantic-release/index.ts
@@ -10,22 +10,29 @@ export default function semanticRelease({
   publishToNpm = false,
   branches = ['main'],
 }: SemanticReleaseConfig) {
+  const npmPlugin = ['@semantic-release/npm', { npmPublish: true }];
+  const githubPlugin = '@semantic-release/github';
+  const gitPlugin = [
+    '@semantic-release/git',
+    {
+      assets: ['package.json'],
+      message:
+        'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
+    },
+  ];
+
+  const plugins: Array<any> = [
+    '@semantic-release/commit-analyzer',
+    '@semantic-release/release-notes-generator',
+  ];
+
+  plugins.push(gitPlugin);
+  if (publishToNpm) plugins.push(npmPlugin);
+  plugins.push(githubPlugin);
+
   return {
     branches,
     extends: isMonorepo ? ['semantic-release-monorepo'] : [],
-    plugins: [
-      '@semantic-release/commit-analyzer',
-      '@semantic-release/release-notes-generator',
-      publishToNpm && ['@semantic-release/npm', { npmPublish: true }],
-      [
-        '@semantic-release/git',
-        {
-          assets: ['package.json'],
-          message:
-            'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
-        },
-      ],
-      '@semantic-release/github',
-    ],
+    plugins,
   };
 }

eb62cbb411f5abddc9682783009b464bcb9c02bb
David Meents <david@allido.io>
2022-08-11 11:30:48 -0700

    fix: revert semantic-release changes

--- src/semantic-release/index.ts
+++ src/semantic-release/index.ts
@@ -10,29 +10,22 @@ export default function semanticRelease({
   publishToNpm = false,
   branches = ['main'],
 }: SemanticReleaseConfig) {
-  const npmPlugin = ['@semantic-release/npm', { npmPublish: true }];
-  const githubPlugin = '@semantic-release/github';
-  const gitPlugin = [
-    '@semantic-release/git',
-    {
-      assets: ['package.json'],
-      message:
-        'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
-    },
-  ];
-
-  const plugins: Array<any> = [
-    '@semantic-release/commit-analyzer',
-    '@semantic-release/release-notes-generator',
-  ];
-
-  if (publishToNpm) plugins.push(npmPlugin);
-  plugins.push(gitPlugin);
-  plugins.push(githubPlugin);
-
   return {
     branches,
     extends: isMonorepo ? ['semantic-release-monorepo'] : [],
-    plugins,
+    plugins: [
+      '@semantic-release/commit-analyzer',
+      '@semantic-release/release-notes-generator',
+      ['@semantic-release/npm', { npmPublish: publishToNpm }],
+      [
+        '@semantic-release/git',
+        {
+          assets: ['package.json'],
+          message:
+            'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
+        },
+      ],
+      '@semantic-release/github',
+    ],
   };
 }

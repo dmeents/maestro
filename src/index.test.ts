476f8ac23ac16d163d7c988fbf394d9a1579168f
David Meents <david@allido.io>
2022-09-20 14:38:26 -0700

    fix: export object

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,3 +1,4 @@
+// @ts-ignore
 import * as Maestro from './index';
 
 describe('Maestro', () => {
--- src/index.ts
+++ src/index.ts
@@ -3,11 +3,4 @@ import jest from './jest';
 import prettier from './prettier';
 import semantic from './semantic-release';
 
-const configurations = {
-  eslint,
-  prettier,
-  semantic,
-  jest,
-};
-
-export default configurations;
+export = { eslint, prettier, semantic, jest };
